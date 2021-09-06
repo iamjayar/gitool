9103cad7d8efaa73245b9c68245025055fc7c91f
mjayar <mddevilleres@gmail.com>
2021-09-06 20:19:34 +0800

    Remove resolve path

--- src/routes/achievements/[id].json.ts
+++ src/routes/achievements/[id].json.ts
@@ -1,7 +1,6 @@
-import { resolve } from "path";
 import type { RequestHandler } from "@sveltejs/kit";
 
 export const get: RequestHandler = async (request) => {
-  const { default: data } = await import(resolve("./src/routes/achievements/lib", `${request.params.id}.json`));
+  const { default: data } = await import(`/src/routes/achievements/lib/${request.params.id}.json`);
   return { body: data };
 };
